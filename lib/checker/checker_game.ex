f00ca4f7f96d0fca8087b861e3f741fe68223f12
Jiangyi Lin <lin.jian@husky.neu.edu>
2018-03-24 11:13:38 -0400

    move finish

--- lib/checker/checker_game.ex
+++ lib/checker/checker_game.ex
@@ -88,6 +88,8 @@ defmodule CheckerWeb.CheckerGame do
           # The from pos is must user's piece.
           # Try to check the value: position, eat piece, become queen
           moves = move_positions(from, game.board_state)
+
+          # in move position has enemies!
           jumps = jump_positions(from, game.board_state)
 
           cond do
@@ -97,7 +99,7 @@ defmodule CheckerWeb.CheckerGame do
             # below have prerequisite: the to_pos is empty
             # it's a move
             to in moves ->
-              game = become_queue(to, game)
+              game = become_queen(to, game)
               # handle update
               try_call(game.id, {:update, game})
               {:ok, game}
@@ -107,8 +109,8 @@ defmodule CheckerWeb.CheckerGame do
             # need to use map for jumps
             to in jumps ->
               # delete eat positions
-              game = delete_pieces(to, jump, game)
-              game = become_queue(to, game)
+              game = delete_pieces(to, jumps, game)
+              game = become_queen(to, game)
               # handle update
               try_call(game.id, {:update, game})
 
@@ -142,10 +144,95 @@ defmodule CheckerWeb.CheckerGame do
   end
 
   # pos: int 0-31, board: list of 32 board unit
+  # this must be right user's right piece
   def move_positions(pos, board) do
     # return list of all valid move position
-    # Enum.at([], 1) -> xx
     x = Enum.at(board, pos)
+    color = String.at(x, 0)
+    len = String.length(x)
+
+    cond do
+      color == 'r' ->
+        # red moves
+        red_moves(pos, len == 2)
+
+      color == 'b' ->
+        # black moves
+        black_moves(pos, len == 2)
+    end
+  end
+
+  # not wrong when it return charlist
+  # can use inspect to see
+  def black_moves(pos, is_queen) do
+    row = div(pos, 4)
+
+    black_moves_alternatives =
+      cond do
+        is_queen ->
+          cond do
+            # 0, 2, 4, 6
+            rem(row, 2) == 0 ->
+              [pos - 4, pos - 3, pos + 4, pos + 5]
+
+            # 1, 3, 5, 7
+            true ->
+              [pos - 5, pos - 4, pos + 3, pos + 4]
+          end
+
+        # normal
+        true ->
+          cond do
+            # 0, 2, 4, 6
+            rem(row, 2) == 0 ->
+              [pos + 4, pos + 5]
+
+            true ->
+              [pos + 3, pos + 4]
+          end
+      end
+
+    # red_moves_alternatives needs a filter
+    Enum.filter(black_moves_alternatives, &pos_filter(&1, 1, pos))
+  end
+
+  def red_moves(pos, is_queen) do
+    row = div(pos, 4)
+
+    red_moves_alternatives =
+      cond do
+        is_queen ->
+          cond do
+            # 0, 2, 4, 6
+            rem(row, 2) == 0 ->
+              [pos - 4, pos - 3, pos + 4, pos + 5]
+
+            # 1, 3, 5, 7
+            true ->
+              [pos - 5, pos - 4, pos + 3, pos + 4]
+          end
+
+        # normal
+        true ->
+          cond do
+            # 0, 2, 4, 6
+            rem(row, 2) == 0 ->
+              [pos - 4, pos - 3]
+
+            # 1, 3, 5, 7
+            true ->
+              [pos - 5, pos - 4]
+          end
+      end
+
+    # red_moves_alternatives needs a filter
+    Enum.filter(red_moves_alternatives, &pos_filter(&1, 1, pos))
+  end
+
+  def pos_filter(pos, dif, ori_pos) do
+    ori_row = div(ori_pos, 4)
+    new_row = div(pos, 4)
+    abs(ori_row - new_row) == dif and pos >= 0 and pos <= 31
   end
 
   # pos: int 0-31, board: list of 32 board unit
