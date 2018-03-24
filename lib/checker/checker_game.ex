a37cb878c09d4f9caf4d6f4ea13a368ef3a45d43
Jiangyi Lin <lin.jian@husky.neu.edu>
2018-03-24 14:09:30 -0400

    jump check_eat get_to_pos check_again check_occupied
    about 4 functions left

--- lib/checker/checker_game.ex
+++ lib/checker/checker_game.ex
@@ -93,8 +93,8 @@ defmodule CheckerWeb.CheckerGame do
           jumps = jump_positions(from, game.board_state)
 
           cond do
-            check_occupied(to) ->
-              {:error, "Invalid step."}
+            check_occupied(to, game.board_state) ->
+              {:error, "Invalid step. Occupied."}
 
             # below have prerequisite: the to_pos is empty
             # it's a move
@@ -106,10 +106,14 @@ defmodule CheckerWeb.CheckerGame do
 
             # it's a jump
             # end_game_check only after jump
-            # need to use map for jumps
+            # need to use map for jumps, and may changed the turn
+            # TODO: use something like find in jump
             to in jumps ->
+              # changed the turn
+
               # delete eat positions
               game = delete_pieces(to, jumps, game)
+              # make moves and perhaps become queen
               game = become_queen(to, game)
               # handle update
               try_call(game.id, {:update, game})
@@ -119,6 +123,10 @@ defmodule CheckerWeb.CheckerGame do
               # then may send a win message to server, server will check
               spawn(__MODULE__, :check_win, [game])
               {:ok, game}
+
+            # cant move or jump to, invalid
+            true ->
+              {:error, "Invalid step. Can't reach"}
           end
       end
   end
@@ -134,7 +142,8 @@ defmodule CheckerWeb.CheckerGame do
   end
 
   # (consider about non-empty)
-  def check_occupied(to) do
+  def check_occupied(to, board) do
+    Enum.at(board, to) != ""
   end
 
   # game is a dict
@@ -144,7 +153,8 @@ defmodule CheckerWeb.CheckerGame do
   end
 
   # pos: int 0-31, board: list of 32 board unit
-  # this must be right user's right piece
+  # prerequisite: this must be right user's right piece
+  # not care about the destination of whether has piece
   def move_positions(pos, board) do
     # return list of all valid move position
     x = Enum.at(board, pos)
@@ -236,9 +246,139 @@ defmodule CheckerWeb.CheckerGame do
   end
 
   # pos: int 0-31, board: list of 32 board unit
+  # not use recursion to avoid a bad bug(same destination, two routes)
+  # when jump and still can jump, set a flag by turn "c"<>pos
+  # will be checked at client
+  # [{is_continue: boolean, to: to, eat: eat_pos},...]
   def jump_positions(pos, board) do
     # return list of all (valid jump, eat position)
     # notice: can still jump after a jump!
+    alternative_moves = move_positions(pos, board)
+    result = Enum.map(alternative_moves, &check_eat(pos, &1, board, false))
+    Enum.filter(result, fn x -> x != nil end)
+  end
+
+  # prerequsite: must can move to eat_pos, x_pos must have a piece
+  # need to check destination valid
+  # {is_continue: boolean, to: to_pos, eat: eat_pos} | nil | true
+  def check_eat(x_pos, eat_pos, board, is_just_check) do
+    # can eat?
+    eat = Enum.at(board, eat_pos)
+    x = Enum.at(board, x_pos)
+
+    cond do
+      # cant eat empty
+      eat == "" ->
+        nil
+
+      # cant use
+      # eat_color = String.at(eat, 0)
+      # x_color = String.at(x, 0)
+
+      # cant eat your piece
+      String.at(eat, 0) == String.at(x, 0) ->
+        nil
+
+      # prerequisite: can eat
+      # can get the destination?
+      # nil for no to_pos || not empty
+      true ->
+        to_pos = get_to_pos(x_pos, eat_pos, board)
+
+        cond do
+          # cant reach destination
+          to_pos == nil ->
+            nil
+
+          # after jump can jump again?
+          true ->
+            if is_just_check do
+              # not check next turn | as a boolean function
+              true
+            else
+              # as a get function
+              cond do
+                # not go back
+                check_again(to_pos, board, eat_pos) ->
+                  %{is_continue: true, to_pos: to_pos, eat_pos: eat_pos}
+
+                # cant jump second time
+                true ->
+                  %{is_continue: false, to_pos: to_pos, eat_pos: eat_pos}
+              end
+            end
+        end
+    end
+  end
+
+  def get_to_pos_temp(x_pos, eat_pos) do
+    row = div(x_pos, 4)
+
+    cond do
+      rem(row, 2) == 0 ->
+        # 0, 2, 4, 6
+        cond do
+          eat_pos - x_pos == 4 ->
+            x_pos + 7
+
+          eat_pos - x_pos == 5 ->
+            x_pos + 9
+
+          eat_pos - x_pos == -4 ->
+            x_pos - 9
+
+          eat_pos - x_pos == -3 ->
+            x_pos - 7
+        end
+
+      true ->
+        # 1 3 5 7
+        cond do
+          eat_pos - x_pos == 3 ->
+            x_pos + 7
+
+          eat_pos - x_pos == 4 ->
+            x_pos + 9
+
+          eat_pos - x_pos == -5 ->
+            x_pos - 9
+
+          eat_pos - x_pos == -4 ->
+            x_pos - 7
+        end
+    end
+  end
+
+  # nil|to_pos
+  def get_to_pos(x_pos, eat_pos, board) do
+    row = div(x_pos, 4)
+    result_temp = get_to_pos_temp(x_pos, eat_pos)
+
+    # check for result_temp should have two lines diff
+    result_row = div(result_temp, 4)
+
+    # scope[0,31] line_diff and empty
+    if result_temp >= 0 and result_temp <= 31 and abs(result_row - row) == 2 and
+         Enum.at(board, result_temp) == "" do
+      result_temp
+    else
+      nil
+    end
+  end
+
+  # def check_eat_boolean(x_pos, eat_pos, board) do
+  #   eat = Enum.at(board, eat_pos)
+  #   x = Enum.at(board, x_pos)
+  # end
+
+  # boolean
+  def check_again(to_pos, board, eaten_pos) do
+    alternative_moves = move_positions(to_pos, board)
+    alternative_moves = Enum.filter(alternative_moves, fn x -> x! = eaten_pos end)
+    # [nil, true,...]
+    results = Enum.map(alternative_moves, &check_eat(to_pos, &1, board, true))
+    # default fn: fn(x) -> x
+    Enum.any?(results)
   end
 
   def get_data(game_id), do: try_call(game_id, :get_data)
